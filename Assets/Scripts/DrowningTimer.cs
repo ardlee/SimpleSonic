f1ecdd02612dc0461d7c5bf6d3db440937a19d3f
corvuscorae <corvuscorae@gmail.com>
2024-04-30 19:16:30 -0700

    unity debug.log()

--- Assets/Scripts/DrowningTimer.cs
+++ Assets/Scripts/DrowningTimer.cs
@@ -37,12 +37,18 @@ public class DrowningTimer : MonoBehaviour
             timer += Time.deltaTime;
             //************* Send the message to the client...
             OSCHandler.Instance.SendMessageToClient("pd", "/unity/tempoDrown", 500/(timer+1)); // sets tempo of drowning sound
-            // Debug.Log();
             if (timer > maxTime) {
                 // Death
                 gameObject.SetActive(false);
             }
         }
+   
+
+    }
+
+
+    void FixedUpdate()
+    {
         //************* Routine for receiving the OSC...
         OSCHandler.Instance.UpdateLogs();
         Dictionary<string, ServerLog> servers = OSCHandler.Instance.Servers;
@@ -54,16 +60,15 @@ public class DrowningTimer : MonoBehaviour
             if (item.Value.log.Count > 0)
             {
                 int lastPacketIndex = item.Value.packets.Count - 1;
-                Debug.Log(lastPacketIndex);
+
                 //get address and data packet
-                //countText.text = item.Value.packets[lastPacketIndex].Address.ToString();
-                //countText.text += item.Value.packets[lastPacketIndex].Data[0].ToString();
+                Debug.Log(item.Value.packets[lastPacketIndex].Data[0].ToString());
 
             }
         }
         //*************
-
     }
+
     /* TODO: can use trigger enter/exit fcns to activate/deactivate drowning sound tempo */
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Water")) {
@@ -72,10 +77,6 @@ public class DrowningTimer : MonoBehaviour
             OSCHandler.Instance.SendMessageToClient("pd", "/unity/tempoDrown", 500); // resets tempo of drowning sound to default (500)
             OSCHandler.Instance.SendMessageToClient("pd", "/unity/triggerDrown", 1); // toggles ON bang object for drowning sound
             //*************
-            //OSCHandler.Instance.Servers.
-            int lastPacket = OSCHandler.Instance.Servers["unity"].packets.Count - 1;
-            //Debug.Log(OSCHandler.Instance.Servers["unity"].packets[lastPacket]);
-            Debug.Log(lastPacket);
         }
     }
 
@@ -84,7 +85,7 @@ public class DrowningTimer : MonoBehaviour
             isUnderwater = false;
             timer = 0;
             //************* Send the message to the client...
-            //OSCHandler.Instance.SendMessageToClient("pd", "/unity/tempoDrown", 0);
+            OSCHandler.Instance.SendMessageToClient("pd", "/unity/tempoDrown", 500);
             OSCHandler.Instance.SendMessageToClient("pd", "/unity/triggerDrown", 0); // toggles OFF bang object for drowning sound
             //*************
         }
