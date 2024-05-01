3da823dbee8b7ae4e1dd33f2f9d07cd6b4076220
corvuscorae <corvuscorae@gmail.com>
2024-04-30 18:55:43 -0700

    4/30 in class update

--- Assets/Scripts/DrowningTimer.cs
+++ Assets/Scripts/DrowningTimer.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityOSC;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class DrowningTimer : MonoBehaviour
 {
@@ -35,15 +37,34 @@ public class DrowningTimer : MonoBehaviour
             timer += Time.deltaTime;
             //************* Send the message to the client...
             OSCHandler.Instance.SendMessageToClient("pd", "/unity/tempoDrown", 500/(timer+1)); // sets tempo of drowning sound
-            //*************
+            // Debug.Log();
             if (timer > maxTime) {
                 // Death
                 gameObject.SetActive(false);
             }
         }
-        
+        //************* Routine for receiving the OSC...
+        OSCHandler.Instance.UpdateLogs();
+        Dictionary<string, ServerLog> servers = OSCHandler.Instance.Servers;
+
+        foreach (KeyValuePair<string, ServerLog> item in servers)
+        {
+            // If we have received at least one packet,
+            // show the last received from the log in the Debug console
+            if (item.Value.log.Count > 0)
+            {
+                int lastPacketIndex = item.Value.packets.Count - 1;
+                Debug.Log(lastPacketIndex);
+                //get address and data packet
+                //countText.text = item.Value.packets[lastPacketIndex].Address.ToString();
+                //countText.text += item.Value.packets[lastPacketIndex].Data[0].ToString();
+
+            }
+        }
+        //*************
+
     }
- /* TODO: can use trigger enter/exit fcns to activate/deactivate drowning sound tempo */
+    /* TODO: can use trigger enter/exit fcns to activate/deactivate drowning sound tempo */
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Water")) {
             isUnderwater = true;
@@ -51,6 +72,10 @@ public class DrowningTimer : MonoBehaviour
             OSCHandler.Instance.SendMessageToClient("pd", "/unity/tempoDrown", 500); // resets tempo of drowning sound to default (500)
             OSCHandler.Instance.SendMessageToClient("pd", "/unity/triggerDrown", 1); // toggles ON bang object for drowning sound
             //*************
+            //OSCHandler.Instance.Servers.
+            int lastPacket = OSCHandler.Instance.Servers["unity"].packets.Count - 1;
+            //Debug.Log(OSCHandler.Instance.Servers["unity"].packets[lastPacket]);
+            Debug.Log(lastPacket);
         }
     }
 
@@ -59,6 +84,7 @@ public class DrowningTimer : MonoBehaviour
             isUnderwater = false;
             timer = 0;
             //************* Send the message to the client...
+            //OSCHandler.Instance.SendMessageToClient("pd", "/unity/tempoDrown", 0);
             OSCHandler.Instance.SendMessageToClient("pd", "/unity/triggerDrown", 0); // toggles OFF bang object for drowning sound
             //*************
         }
